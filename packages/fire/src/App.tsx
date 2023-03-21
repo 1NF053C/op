0d73c6e3d1ebbd5de3a278ad8f98b2e95c80c717
1nf053c <1nf053c>
2023-03-20 23:27:17 -0400

    init file package: reactjs + vitejs + typescript + mui + emotion styled

---
+++ packages/fire/src/App.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+const Root = styled.div`
+  background-color: black;
+  color: white;
+`
+
+function App() {
+  return (
+    <Root>
+      Todo
+    </Root>
+  );
+}
+
+export default App;
